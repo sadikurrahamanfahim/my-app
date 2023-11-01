a6038046944eed34b7eafb78c3573119c31626aa
muhammad fahim <fahim99369@gmail.com>
2023-11-01 16:22:32 +0600

    about with dark mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-// import Textform from './components/Textform';
+import Textform from './components/Textform';
 import About from './components/About';
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" about="About me" />
-      {/*<div className="container my-3"><Textform heading="Enter the text to make UpperCase" /></div>*/}
+      {<div className="container my-3"><Textform heading="Enter the text to make UpperCase" /></div>}
       <div className="container my-3"><About/></div>
     </>
   );
