3a4021f6089579abd2aec378858fa25f64374c46
muhammad fahim <training.octbd@gmail.com>
2024-05-03 21:28:39 +0600

    up

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 import About from './components/About';
 import { useState } from 'react';
+import Alert from './components/Alert';
 
 function App() {
   const[mode, setMode] = useState('light');//wheather dark mode is enbled or not
+  
+  const[alert, setAlert] = useState(null);
   
   const toggleMode = ()=>{
     if(mode==='light'){
@@ -21,6 +24,7 @@ function App() {
   return (
     <>
       <Navbar title="TextUtils" about="About me" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert="This is alert"/>
       {<div className="container my-3"><Textform heading="Enter the text to make UpperCase" mode={mode} /></div>}
       <div className="container my-3"><About /></div>
     </>
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Alert() {
     return (
         <div className="alert alert-warning alert-dismissible fade show" role="alert">
-            <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+            <strong>{alert}</strong>
             <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     )
